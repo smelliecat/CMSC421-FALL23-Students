399029e0bcf0989cad08f62df3b5d91722c03f73
Kwesi <kwecobi@gmail.com>
2023-09-27 12:06:49 -0400

    Added q3_a_(l & nl) and q3_b_(l & nl) data to the generator to so you don't have to worry about modifying the continous data

--- Assignment_1/Data/generator.py
+++ Assignment_1/Data/generator.py
@@ -1,5 +1,7 @@
 # Test 1
 import numpy as np
+np.random.seed(42)
+
 def train_test_split(x, y, test_size=0.2):
 
     # Split data
@@ -19,7 +21,7 @@ def train_test_split(x, y, test_size=0.2):
     return x_train, x_test, y_train, y_test
   
 
-def generate_data(num_samples=100, dimension=1, test_size=0.2, m=7, b=3, low=0, high=10):
+def generate_data(num_samples=100, dimension=1, test_size=0.2, m=7, b=3, low=0, high=10, categorical=False):
 
     # Generate x values
     x = np.random.uniform(low=low, high=high, size=(num_samples, dimension))
@@ -31,22 +33,28 @@ def generate_data(num_samples=100, dimension=1, test_size=0.2, m=7, b=3, low=0, 
         y = (m * x) + b + noise
     
     else:
-    #     noise = np.random.normal(loc=0, scale=1, size=num_samples)
-    #     y = np.dot(x, np.array([m]*dimension)) + b + noise
-
         noise = np.random.normal(loc=0, scale=1, size=num_samples)
         y = np.dot(x, np.array([m] * dimension)) + b + noise
 
     y = y.reshape(-1, 1)  # Make y a column vector
 
+    if categorical:
+        quartiles = np.percentile(y, [25, 50, 75])
+        labels = np.zeros(num_samples, dtype=int)
+        labels[y[:, 0] <= quartiles[0]] = 0
+        labels[(y[:, 0] > quartiles[0]) & (y[:, 0] <= quartiles[1])] = 1
+        labels[(y[:, 0] > quartiles[1]) & (y[:, 0] <= quartiles[2])] = 2
+        labels[y[:, 0] > quartiles[2]] = 3
+        y = labels
+        y = y.reshape(-1, 1)
+        
     # Split data into train/test
     xtrain, xtest, ytrain, ytest = train_test_split(x, y, test_size)
 
     return xtrain, xtest, ytrain, ytest
 
 
-
-def generate_sine_data(num_samples=100, dimension=1, test_size=0.2, amplitude=1, frequency=1, phase=0, low=0, high=10):
+def generate_sine_data(num_samples=100, dimension=1, test_size=0.2, amplitude=1, frequency=1, phase=0, low=0, high=10, categorical=False):
     x = np.random.uniform(low=low, high=high, size=(num_samples, dimension))
 
     if dimension == 1:
@@ -56,10 +64,15 @@ def generate_sine_data(num_samples=100, dimension=1, test_size=0.2, amplitude=1,
         for d in range(dimension):
             y += amplitude * np.sin(frequency * x[:, d].reshape(-1, 1) + phase)
 
-    # x = (x - np.mean(x)) / np.std(x)
-    # y = (y - np.mean(y)) / np.std(y)
     print(max(y))
     print(min(y))
+
+    if categorical:
+        # Define bins for y values
+            bins = [np.min(y), np.percentile(y, 33), np.percentile(y, 66), np.max(y)]
+            
+            # Convert y values to categorical labels
+            y = np.digitize(y, bins) - 1
     x_train, x_test, y_train, y_test = train_test_split(x, y, test_size)
 
     return np.array(x_train), np.array(x_test), np.array(y_train), np.array(y_test)
@@ -107,6 +120,17 @@ def q2_b():
     xtrain, xtest, ytrain, ytest = generate_sine_data(num_samples=100000, dimension=5, test_size=0.2, low=0, high=10)
     xtrain, xtest, ytrain, ytest = np.array(xtrain, dtype=np.float32), np.array(xtest, dtype=np.float32), np.array(ytrain, dtype=np.float32), np.array(ytest, dtype=np.float32)
     print(xtrain.shape, xtest.shape, ytrain.shape, ytest.shape)
+    print(xtrain)
+    print(ytrain)
+    return {
+        "train": (xtrain, ytrain),
+        "test": (xtest, ytest)
+    }
+
+def q3_a_l():
+    xtrain, xtest, ytrain, ytest = generate_data(num_samples=10000, test_size=0.2, categorical=True)
+    xtrain, xtest, ytrain, ytest = np.array(xtrain, dtype=np.float32), np.array(xtest, dtype=np.float32), np.array(ytrain, dtype=np.float32), np.array(ytest, dtype=np.float32)
+    print(xtrain.shape, xtest.shape, ytrain.shape, ytest.shape)
     # print(xtrain)
     # print(ytrain)
     return {
@@ -114,9 +138,46 @@ def q2_b():
         "test": (xtest, ytest)
     }
 
+def q3_a_nl():
+    xtrain, xtest, ytrain, ytest = generate_sine_data(num_samples=10000, dimension=1, test_size=0.2,categorical=True)
+    xtrain, xtest, ytrain, ytest = np.array(xtrain, dtype=np.float32), np.array(xtest, dtype=np.float32), np.array(ytrain, dtype=np.float32), np.array(ytest, dtype=np.float32)
+    print(xtrain.shape, xtest.shape, ytrain.shape, ytest.shape)
+    # print(xtrain)
+    # print(ytrain)
+    return {
+        "train": (xtrain, ytrain),
+        "test": (xtest, ytest)
+    }
+
+def q3_b_l():
+    xtrain, xtest, ytrain, ytest = generate_data(num_samples=10000, dimension=4, test_size=0.2, low=0, high=10, categorical=True)
+    xtrain, xtest, ytrain, ytest = np.array(xtrain, dtype=np.float32), np.array(xtest, dtype=np.float32), np.array(ytrain, dtype=np.float32), np.array(ytest, dtype=np.float32)
+    print(xtrain.shape, xtest.shape, ytrain.shape, ytest.shape)
+    # print(xtrain)
+    # print(ytrain)
+    return {
+        "train": (xtrain, ytrain),
+        "test": (xtest, ytest)
+    }
+
+def q3_b_nl():
+    xtrain, xtest, ytrain, ytest = generate_sine_data(num_samples=100000, dimension=5, test_size=0.2, low=0, high=10, categorical=True)
+    xtrain, xtest, ytrain, ytest = np.array(xtrain, dtype=np.float32), np.array(xtest, dtype=np.float32), np.array(ytrain, dtype=np.float32), np.array(ytest, dtype=np.float32)
+    print(xtrain.shape, xtest.shape, ytrain.shape, ytest.shape)
+    # print(xtrain)
+    # print(ytrain)
+    return {
+        "train": (xtrain, ytrain),
+        "test": (xtest, ytest)
+    }
 q1_a()
 q2_a()
-
+q3_a_l()
+q3_a_nl()
 print("-----------------------------------------")
 q1_b()
 q2_b()
+q3_b_l()
+q3_b_nl()
+
+
